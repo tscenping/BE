3f5f2c1cc8115be16e9a926e140fcb6b9fb5b7eb
jiyun <tomatoziyun@gmail.com>
2023-11-03 18:18:04 +0900

    feat: typeOrmConfig 작성 및 import

---
+++ src/common/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [__dirname + '/../../**/entities/*.entity.{js,ts}'],
+  synchronize: false,
+};
