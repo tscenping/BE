9f3e7e5942bfbd99a3b3a6e7349d9d5be7dc3199
cjho0316 <cjho03160316@gmail.com>
2023-11-27 13:28:04 +0900

    feat(Redis): Delete unnecessary redis module

--- src/redis/dto/update-redi.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRediDto } from './create-redi.dto';
-
-export class UpdateRediDto extends PartialType(CreateRediDto) {}
--- src/redis/redis.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisService } from './redis.service';
-import { RedisController } from './redis.controller';
-
-@Module({
-  controllers: [RedisController],
-  providers: [RedisService]
-})
-export class RedisModule {}
