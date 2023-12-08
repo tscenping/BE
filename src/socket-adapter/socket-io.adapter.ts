0f56591f81159e3bb32b1dca293a8b36f25bdff5
jiyun <tomatoziyun@gmail.com>
2023-12-08 18:25:24 +0900

    fix: 필요없는 부분 제거

--- src/socket-adapter/socket-io.adapter.ts
+++ src/socket-adapter/socket-io.adapter.ts
@@ -1,11 +1,7 @@
 import { INestApplicationContext } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { Server, ServerOptions, Socket } from 'socket.io';
-import { WSUnauthorizedException } from '../common/exception/custom-exception';
 import { User } from '../users/entities/user.entity';
-import { UsersRepository } from '../users/users.repository';
 
 type JwtPayload = {
 	user: User;
@@ -19,9 +15,6 @@ export class SocketIoAdapter extends IoAdapter {
 	}
 
 	createIOServer(port: number, options?: any) {
-		// const jwtService = this.app.get(JwtService);
-		// const configService = this.app.get(ConfigService);
-		// const userRepository = this.app.get(UsersRepository);
 		const clientPort = '8001';
 
 		const cors = {
@@ -40,51 +33,6 @@ export class SocketIoAdapter extends IoAdapter {
 		};
 
 		const server: Server = super.createIOServer(port, optionsWithCORS);
-
-		// const namespaces = ['channels'];
-		//
-		// namespaces.forEach((namespace) => {
-		// 	server
-		// 		.of(namespace)
-		// 		.use(
-		// 			wsAuthGuardMiddleware(
-		// 				jwtService,
-		// 				configService,
-		// 				userRepository,
-		// 			),
-		// 		);
-		// });
 		return server;
 	}
 }
-
-const wsAuthGuardMiddleware =
-	(
-		jwtService: JwtService,
-		configService: ConfigService,
-		usersRepository: UsersRepository,
-	) =>
-	async (socket: SocketWithAuth, next: any) => {
-		// const token = socket.handshake.auth.token;
-		const cookie = socket.handshake.headers.cookie;
-		if (!cookie) return next(WSUnauthorizedException('no cookie'));
-
-		const accessToken = cookie.split(';')[0].split('=')[1];
-		if (!accessToken)
-			return next(WSUnauthorizedException('no access token'));
-		// accessToken 유효성 검사
-
-		const payload = await jwtService.verifyAsync(accessToken, {
-			secret: configService.get<string>('JWT_SECRET'),
-		});
-		if (!payload) return next(WSUnauthorizedException('no payload'));
-
-		const user = await usersRepository.findOne({
-			where: { id: payload.id },
-		});
-		if (!user) return next(WSUnauthorizedException('no user'));
-		console.log('user:', JSON.stringify(user));
-
-		socket.user = user;
-		next();
-	};
