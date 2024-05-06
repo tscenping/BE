edbb79193d5998aae5dc656c1c56093d7e246c4d
yubinquitous <yb9854@naver.com>
2024-05-06 16:17:12 +0900

    [CHORE] BE-config 업데이트 및 health api 수정

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,14 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('health')
 @ApiTags('health')
 export class HealthController {
+	@Get('/')
 	@ApiOperation({
 		summary: '헬스 체크',
 		description: '서버의 상태를 체크합니다.',
 	})
-	async healthCheck() {
+	healthCheck() {
 		return 'ok';
 	}
 }
